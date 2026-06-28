2bf7436e5e01f024652bbb63046cb559dd187afc
khanjanshah2006 <khanjanshah2006@gmail.com>
2026-06-29 01:27:40 +0530

    Time: 51 ms (43.58%), Space: 51 MB (20.11%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0]++;
+        int sum = 0;
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+            ans += mp[sum - k];
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
