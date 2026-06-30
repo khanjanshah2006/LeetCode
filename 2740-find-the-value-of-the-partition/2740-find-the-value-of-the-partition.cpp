d12408da909cc611b24d2be583dcd5d02c6ad557
khanjanshah2006 <khanjanshah2006@gmail.com>
2026-06-30 23:57:06 +0530

    Time: 20 ms (45.6%), Space: 63.8 MB (49.2%) - LeetHub

---
+++ 2740-find-the-value-of-the-partition/2740-find-the-value-of-the-partition.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findValueOfPartition(vector<int>& nums) {
+        int ans = INT_MAX;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i = 0; i < n-1; i++) {
+            ans = min(ans, nums[i+1]-nums[i]);
+        }
+        return ans;
+    }
+};
