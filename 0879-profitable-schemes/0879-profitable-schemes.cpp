e24f15752f948a81aed37d3716f3dba6e7b95a4b
khanjanshah2006 <khanjanshah2006@gmail.com>
2026-07-04 01:08:41 +0530

    Time: 187 ms (18.74%), Space: 56.7 MB (20.16%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,32 +1,37 @@
-class Solution {
-public:
-    int N;
+class Solution
+{
+    public:
+        int N;
     long long mod = 1e9 + 7;
-    vector<vector<vector<int>>> dp;
+    vector<vector<vector< int>>> dp;
     long long solve(int i, int remPeople, int curProfit,
-                    vector<int>& group, vector<int>& profit, int& minProfit) {
-        if (i == N || remPeople == 0) {
+        vector<int> &group, vector<int> &profit, int &minProfit)
+    {
+        if (i == N || remPeople == 0)
+        {
             return (curProfit >= minProfit) ? 1 : 0;
         }
         if (dp[i][remPeople][curProfit] != -1)
             return dp[i][remPeople][curProfit];
         long long take = 0;
-        if (group[i] <= remPeople) {
+        if (group[i] <= remPeople)
+        {
             int newProfit = min(minProfit, curProfit + profit[i]);
             take = solve(i + 1, remPeople - group[i], newProfit, group, profit,
-                         minProfit) %
-                   mod;
+                    minProfit) %
+                mod;
         }
         long long notTake =
             solve(i + 1, remPeople, curProfit, group, profit, minProfit) % mod;
         return dp[i][remPeople][curProfit] = ((take + notTake) % mod);
     }
 
-    int profitableSchemes(int n, int minProfit, vector<int>& group,
-                          vector<int>& profit) {
+    int profitableSchemes(int n, int minProfit, vector<int> &group,
+        vector<int> &profit)
+    {
         N = group.size();
         dp.assign(N + 1,
-                  vector<vector<int>>(n + 1, vector<int>(minProfit + 1, -1)));
+            vector<vector < int>> (n + 1, vector<int> (minProfit + 1, -1)));
         return solve(0, n, 0, group, profit, minProfit);
     }
 };
