ab2d6b3ef05614b6afbd4a0ac5ff92033dcf4e28
khanjanshah2006 <khanjanshah2006@gmail.com>
2026-07-05 23:19:28 +0530

    Time: 4 ms (18.99%), Space: 16.1 MB (63.45%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>> dp;
-    int solve(int i, int state, vector<int>& prices) {
-        if(i == n) return 0;
-        if(dp[i][state] != -1) return dp[i][state];
-        int ans;
-        if(state == 0) {
-            int case1 = prices[i] + solve(i+1,1,prices);
-            int case2 = solve(i+1,0,prices);
-            ans = max(case1,case2);
-        }else if (state == 1) {
-            ans = solve(i+1,2,prices);
-        }else {
-            int case1 = -prices[i] + solve(i+1,0,prices);
-            int case2 = solve(i+1,2,prices);
-            ans = max(case1, case2);
-        }
-        return dp[i][state] = ans;
-    }
-
+    
     int maxProfit(vector<int>& prices) {
-        n = prices.size();
-        dp.assign(n+1, vector<int>(3,-1));
-        return solve(0,2, prices);
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(3,0));
+        for(int i = n-1; i >= 0; i--) {
+            for(int state = 0; state < 3; state++) {
+                int ans;
+                if(state == 0) {
+                    int case1 = prices[i] + dp[i+1][1];
+                    int case2 = dp[i+1][0];
+                    ans = max(case1,case2);
+                }else if (state == 1) {
+                    ans = dp[i+1][2];
+                } else {
+                    int case1 = -prices[i] + dp[i+1][0];
+                    int case2 = dp[i+1][2];
+                    ans = max(case1,case2);
+                }
+                dp[i][state] = ans;
+            }
+        }
+        return dp[0][2];
     }
 };
