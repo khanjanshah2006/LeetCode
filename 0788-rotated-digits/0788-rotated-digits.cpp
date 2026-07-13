5987be830d058faf4a971a197f5a542f4febf831
khanjanshah2006 <khanjanshah2006@gmail.com>
2026-07-13 16:42:47 +0530

    Time: 0 ms (100%), Space: 8 MB (42.84%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    bool check(int n) {
-        bool flag = false;
-        while(n > 0) {
-            int digit = n%10;
-            if(digit == 3 || digit == 4 || digit == 7) return false;
-            if(digit == 2 || digit == 5 || digit == 6 || digit == 9) {
-                flag = true;
-            }
-            n = n/10;
-        }
-        return flag;
-    }
-    int rotatedDigits(int n) {
+    int dp[6][2][2];
+    string num;
+    int solve(int i, bool tight, bool changed) {
+        if(i == num.size()) return changed;
+        if(dp[i][tight][changed] != -1) return dp[i][tight][changed];
+        
         int ans = 0;
-        for(int i = 1; i <= n; i++) {
-            if(check(i)) ans++;
+        int limit = tight ? num[i]-'0' : 9;
+        for(int digit = 0; digit <= limit; digit++) {
+            bool newtight = tight && (digit == limit);
+            if(digit == 3 || digit == 4 || digit == 7) continue;
+            bool flag = (digit == 2 || digit == 5 || digit == 6 || digit == 9);
+            ans += solve(i+1,newtight, changed || flag);
         }
-        return ans;
+        return dp[i][tight][changed] = ans;
+    }
+
+    int rotatedDigits(int n) {
+        num = to_string(n);
+        memset(dp,-1,sizeof(dp));
+        return solve(0,true,false);
     }
 };
