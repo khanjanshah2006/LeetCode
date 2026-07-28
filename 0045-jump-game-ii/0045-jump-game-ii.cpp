6006253bc2709a28da4d0ee9cc189985409606e7
khanjanshah2006 <khanjanshah2006@gmail.com>
2026-07-28 15:22:46 +0530

    Time: 0 ms (100%), Space: 20.5 MB (73.06%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int n;
-    vector<int> dp;
-    int solve(int i, vector<int>& nums) {
-        if(i >= n) return INT_MAX;
-        if(i == n-1) return 0;
-        if(dp[i] != -1) return dp[i];
-        int ans = INT_MAX;
-        for(int j = 1; j <= nums[i]; j++) {
-            int jump = solve(i+j,nums);
-            if(jump != INT_MAX) {
-                ans = min(ans,1+jump);
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int maxend = 0;
+        int currend = 0;
+        int ans = 0;
+        for(int i = 0; i < n-1; i++) {
+            maxend = max(maxend, i+nums[i]);
+            if(i == currend) {
+                ans++;
+                currend = maxend;
             }
         }
-        return dp[i] = ans;
-    }
-
-    int jump(vector<int>& nums) {
-        n = nums.size();
-        dp.assign(n+1,-1);
-        return solve(0,nums);
+        return ans;
     }
 };
