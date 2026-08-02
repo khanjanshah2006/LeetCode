a054f2dadfcf494a96610531551aa47de1563a95
khanjanshah2006 <khanjanshah2006@gmail.com>
2026-08-02 14:53:14 +0530

    Time: 39 ms (42.98%), Space: 140.7 MB (56.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int n;
     vector<int> dp;
-    int solve(int i, vector<int>& stoneValue) {
+    int solve(int i, vector<int>& arr) {
         if(i >= n) return 0;
         if(dp[i] != -1) return dp[i];
-        int result = stoneValue[i] - solve(i+1,stoneValue);
-        if(i + 1 < n) {
-            result = max(result, stoneValue[i] + stoneValue[i+1] - solve(i+2,stoneValue));
-        }
-        if(i + 2 < n) {
-            result = max(result, stoneValue[i]+stoneValue[i+1]+stoneValue[i+2] - solve(i+3,stoneValue));
-        }
-        return dp[i] = result;
 
+        int sum = 0;
+        int ans = INT_MIN;
+        for(int x = 1; x <= 3; x++) {
+            if(i+x-1 == n) break;
+            sum += arr[i+x-1];
+            ans = max(ans, sum - solve(i+x,arr));
+        }
+        return dp[i] = ans;
     }
-
 
     string stoneGameIII(vector<int>& stoneValue) {
         n = stoneValue.size();
         dp.assign(n+1,-1);
-        int diff = solve(0,stoneValue);
-        if(diff > 0) return "Alice";
-        else if(diff < 0) return "Bob";
-        return "Tie";
+        int ans = solve(0,stoneValue);
+        if(ans == 0) return "Tie";
+        else if(ans > 0) return "Alice";
+        return "Bob";
     }
 };
