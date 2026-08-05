d2b2d8345ea2cc6bc6a3c7b8dd0b3efb53d91ba0
khanjanshah2006 <khanjanshah2006@gmail.com>
2026-08-05 12:43:49 +0530

    Time: 173 ms (70.34%), Space: 304.8 MB (70.34%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -12,19 +12,18 @@ public:
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
         vector<bool> visited(n,false);
         vector<vector<int>> adj(n);
-        vector<vector<int>> adjR(n);
         for(auto& edge: invocations) {
         	int u = edge[0];
         	int v = edge[1];
         	adj[u].push_back(v);
-        	adjR[v].push_back(u);
         }
+
         dfs(k,adj,visited);
 
         for(int u = 0; u < n; u++) {
-            if(!visited[u]) continue;
-        	for(auto& v: adjR[u]) {
-        		if(!visited[v]) {
+            if(visited[u]) continue;
+        	for(auto& v: adj[u]) {
+        		if(visited[v]) {
                     vector<int> arr(n);
                     for(int i=0; i < n; i++) arr[i] = i;
                     return arr;
