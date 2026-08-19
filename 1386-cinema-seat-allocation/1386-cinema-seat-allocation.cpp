4be4a779e14c391f85a53f7f051918952097466e
khanjanshah2006 <khanjanshah2006@gmail.com>
2026-08-19 15:45:21 +0530

    Time: 39 ms (56.25%), Space: 64.1 MB (53.89%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int, vector<int>> reserved;
+        unordered_map<int,int> reserved;
         for(auto& seat: reservedSeats) {
             int r = seat[0];
             int s = seat[1];
-            reserved[r].push_back(s);
+            reserved[r] |= (1 << s);
         }
         int ans = (n - reserved.size())*2;
         for(auto& it: reserved) {
-            vector<int> state(3,true);
-            for(int seat: it.second) {
-                if(seat == 2 || seat == 3) state[0] = false;
-                else if(seat == 8 || seat == 9) state[2] = false;
-                else if(seat == 4 || seat == 5) state[0] = state[1] = false;
-                else if(seat == 6 || seat == 7) state[1] = state[2] = false;
-            }
+            vector<bool> state(3,true);
+            state[0] = !(it.second & (0b0000111100));
+            state[1] = !(it.second & (0b0011110000));
+            state[2] = !(it.second & (0b1111000000));
             if(state[0] && state[2]) ans += 2;
             else if(state[0] || state[1] || state[2]) ans += 1;
         }
